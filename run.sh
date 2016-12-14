562edbcc1e94c162756854fc1ad3708cf9111bb5
YeFengLee <2684498887@qq.com>
2016-12-14 22:23:38 +0800

    bug

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 touch /cron.log
 tail -F /cron.log &
 
-echo "${CRON} >> /cron.log 2>&1" > /crontab.conf
-crontab  /crontab.conf
+echo "${CRON} >> /cron.log 2>&1" >> /etc/crontab
+crontab  /etc/crontab
 echo "=> Running cron job"
 exec cron -f
